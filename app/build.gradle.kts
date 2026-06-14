2cf292387c2fcd5eaea2f77506bc3fd406338419
Lourdes RM <moronlu18@gmail.com>
2026-06-14 20:44:31 +0200

    build: update Gradle configuration and dependencies
    - Update build.gradle.kts with Firebase and Dokka plugins
    - Update version catalog and Gradle wrapper properties
    - Sync settings.gradle.kts with new plugin aliases

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,15 @@
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("org.jetbrains.dokka")
+    id("com.google.gms.google-services")
+    // Add the Crashlytics Gradle plugin
+    id("com.google.firebase.crashlytics")
 }
 
 android {
     namespace = "com.moronlu18.helloworld"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.moronlu18.helloworld"
@@ -20,23 +24,51 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-
 }
 
+
+tasks.dokkaHtml.configure {
+    outputDirectory.set(file("../documentation/html"))
+}
+/*
+tasks.dokkaJavadoc.configure {
+    outputDirectory.set(file("../documentation/javadoc"))
+}*/
 dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
-    implementation(files("/home/lourdes/Android/Sdk/platforms/android-33/android.jar"))
+    implementation(libs.androidx.core.ktx)
+    //Para documentar el proyecto con javadoc se debe añadir la siguiente librería
+    //implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    //implementation(files("/home/lourdes/Android/Sdk/platforms/android-33/android.jar"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    //Librería para documentar con Dokka
+    dokkaPlugin("org.jetbrains.dokka:android-documentation-plugin:1.9.20")
+    // Is applied universally
+    dokkaPlugin("org.jetbrains.dokka:mathjax-plugin:1.9.20")
+    // Is applied for the single-module dokkaHtml task only
+    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.9.20")
+    // Is applied for HTML format in multi-project builds
+    dokkaHtmlPartialPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.9.20")
+
+    //Firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
+    implementation("com.google.firebase:firebase-crashlytics")
+    implementation("com.google.firebase:firebase-analytics")
+
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,31 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+import org.jetbrains.dokka.base.DokkaBase
+import org.jetbrains.dokka.base.DokkaBaseConfiguration
+import org.jetbrains.dokka.gradle.DokkaTask
+
 plugins {
     alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id("org.jetbrains.dokka") version "1.9.20"
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    // Add the dependency for the Crashlytics Gradle plugin
+    id("com.google.firebase.crashlytics") version "2.9.9" apply false
 }
-true // Needed to make the Suppress annotation work for the plugins block
+
+tasks.withType<DokkaTask>().configureEach {
+    pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
+        //customAssets = listOf(file("my-image.png"))
+        //customStyleSheets = listOf(file("my-styles.css"))
+        footerMessage = "(c) 2022 Lourdes Rodríguez Morón"
+        separateInheritedMembers = false
+        //templatesDir = file("dokka/templates")
+        mergeImplicitExpectActualDeclarations = false
+    }
+}
+
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.dokka:dokka-base:1.9.20")
+    }
+}
+
+
